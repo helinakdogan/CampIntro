678119d241afab5b831fd8d5e46dee8b886eec75
Helin Akdoğan <akdoganhelin@gmail.com>
2021-06-14 02:51:56 +0300

    add

---
+++ Odev1/MusteriManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odev1
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri) 
+        {
+            Console.WriteLine("Müşteri eklendi : " + musteri.Adi + " " + musteri.Soyadi);      
+        }
+
+        public void Silme(Musteri musteri) 
+        {
+            Console.WriteLine("Müşteri silindi : " + musteri.Adi + " " + musteri.Soyadi);              
+        }
+
+        public void Listele(params Musteri[] musteriler)
+        {
+            Console.WriteLine(          );
+            Console.WriteLine("Müşteri Listesi");
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine("-" + musteri.Adi + " " + musteri.Soyadi);
+            }       
+        }
+
+
+
+
+    }
+}
---
+++ Odev1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Odev1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+            Musteri musteri1 = new Musteri();
+            musteri1.Adi = "Ali";
+            musteri1.Soyadi = "Yılmaz";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Adi = "Ece";
+            musteri2.Soyadi = "Özkan";
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            musteriManager.Silme(musteri1);
+            musteriManager.Ekle(musteri1); 
+            musteriManager.Listele(musteri1, musteri2);
+        }
+    }
+}
